43d79fac313b267279e0f1a22037883758f40f5a
RustySoulz <96266396+RustySoulz@users.noreply.github.com>
2022-07-02 18:31:40 +0100

    Very Basic Working Game
    Simple console UI with randomly generated questions!

--- NHIEConsoleAPP.cpp
+++ NHIEConsoleAPP.cpp
@@ -4,24 +4,54 @@
 #include <vector>
 #include <ctime>
 
+void InitConsoleApp(std::string FileName);
 std::vector<std::string> FileToVector(std::string FileName);
 std::string RandomStringFromVector(std::vector<std::string> VectorName);
 
 int main()  //Use main method to test or initiate other methods, do not write any code in here that you want to keep!
 {
-    std::vector<std::string> QuestionList = FileToVector("./questions.txt");
-    std::string exit;
-
-    while (exit != "stop")
-    {
-        std::cout << RandomStringFromVector(QuestionList) << std::endl;
-        std::cin >> exit;
-    }
+    InitConsoleApp("./questions.txt");
 
     return 0;
 }
 
 //Put methods here: (Please write a brief description of what they are supposed to do and any headers they require)
+
+void InitConsoleApp(std::string FileName)   //Starts the console application. ()[]
+{
+    std::cout << "****** NEVER HAVE I EVER v0.1 by RustySoul ******" << std::endl;
+    std::cout << std::endl;
+
+    bool Exit = false;
+    while (!Exit)
+    {
+        std::vector<std::string> QuestionList = FileToVector(FileName);
+    
+        std::cout << "Please select an option using 0-9 on the keyboard:" << std::endl;
+
+        std::cout << "1. Get random question" << std::endl;
+        std::cout << "0. Exit Game" << std::endl;
+
+        int Input;
+        std::cin >> Input;
+        std::cout << std::endl;
+
+        switch (Input)
+        {
+        case 0:
+            Exit = true;
+            break;
+
+        case 1:
+            std::cout << RandomStringFromVector(QuestionList) << std::endl;
+            break;
+
+        default:
+            break;
+        }
+    std::cout << std::endl;
+    }
+}
 
 std::vector<std::string> FileToVector(std::string FileName) //Converts a text file containing a list of strings into a vector of strings. (iostream, fstream, string, vector)
 {
@@ -36,7 +66,7 @@ std::vector<std::string> FileToVector(std::string FileName) //Converts a text fi
 
         myFile.close();
     }
-    if (!myFile.is_open())
+    else
     {
         std::cout << "The file called " << FileName << " did not open!" << std::endl;
     }
@@ -44,7 +74,7 @@ std::vector<std::string> FileToVector(std::string FileName) //Converts a text fi
     return lines;
 }
 
-std::string RandomStringFromVector(std::vector<std::string> VectorName)    //Takes a vector of strings and outputs a random string from the list. ()
+std::string RandomStringFromVector(std::vector<std::string> VectorName)    //Takes a vector of strings and outputs a random string from the list. (ctime, string, vector) *Left as method as can be made more random later!
 {
     std::string RandomString;
     srand(time(NULL)*VectorName.size());
